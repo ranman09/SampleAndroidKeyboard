c3eacf3cf6e5ffc96db4ee7195d7fc4a5d962aae
ranman09 <>
2019-09-21 15:42:35 +0800

    created a functional app with tag unicode. but unicode font only works in app

--- app/src/main/java/com/codingyogurt/samplekeyboard/MyInputMethodService.java
+++ app/src/main/java/com/codingyogurt/samplekeyboard/MyInputMethodService.java
@@ -4,41 +4,37 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
 
+
 public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
-
-    private KeyboardView keyboardView;
-    private Keyboard keyboard;
-
-    private boolean caps = false;
 
     @Override
     public View onCreateInputView() {
-        keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
-        keyboard = new Keyboard(this, R.xml.keys_layout);
+        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.keys_layout);
         keyboardView.setKeyboard(keyboard);
         keyboardView.setOnKeyboardActionListener(this);
         return keyboardView;
     }
 
     @Override
-    public void onPress(int primaryCode) {
+    public void onPress(int i) {
 
     }
 
     @Override
-    public void onRelease(int primaryCode) {
+    public void onRelease(int i) {
 
     }
 
     @Override
-    public void onKey(int primaryCode, int[] keyCodes) {
+    public void onKey(int primatyCode, int[] keyCodes) {
         InputConnection inputConnection = getCurrentInputConnection();
+
         if (inputConnection != null) {
-            switch(primaryCode) {
+            switch(primatyCode) {
                 case Keyboard.KEYCODE_DELETE :
                     CharSequence selectedText = inputConnection.getSelectedText(0);
 
@@ -47,29 +43,17 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
                     } else {
                         inputConnection.commitText("", 1);
                     }
-                case Keyboard.KEYCODE_SHIFT:
-                    caps = !caps;
-                    keyboard.setShifted(caps);
-                    keyboardView.invalidateAllKeys();
-                    break;
-                case Keyboard.KEYCODE_DONE:
-                    inputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
 
                     break;
                 default :
-                    char code = (char) primaryCode;
-                    if(Character.isLetter(code) && caps){
-                        code = Character.toUpperCase(code);
-                    }
+                    char code = (char) primatyCode;
                     inputConnection.commitText(String.valueOf(code), 1);
-
             }
         }
-
     }
 
     @Override
-    public void onText(CharSequence text) {
+    public void onText(CharSequence charSequence) {
 
     }
 
@@ -92,6 +76,4 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
     public void swipeUp() {
 
     }
-
-
 }
